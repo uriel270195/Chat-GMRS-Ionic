e1b72d84666578e0a8db9f641bb0f38e00ed92f3
uriel270195 <uriel_270195@hotmail.com>
2018-10-26 10:16:13 -0500

    chat GMRS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDQQBdi7PPOF0vcDmH1wXiNCAkMHTW2Psk",
+  authDomain: "chat-gmrs.firebaseapp.com",
+  databaseURL: "https://chat-gmrs.firebaseio.com",
+  projectId: "chat-gmrs",
+  storageBucket: "chat-gmrs.appspot.com",
+  messagingSenderId: "1016682480653"
+};
 
 @Component({
   selector: 'app-root',
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild("content") content: any;
+  userName: string = '';
+  message: string = '';
+  messages = [];
 
+  constructor(public navCtrl: NavController){
+    this.getMessages();
+  }
+
+  getMessages(){
+    var messagesRef = firebase.database().ref().child("mensajes");
+    messagesRef.on("value",(snap) =>{
+      var data=snap.val();
+      this,this.messages = [];
+      for(var key in data){
+        this.messages.push(data[key]);
+      }
+      this.scrollToBottom();
+    });
+  }
+
+  sentMessage(){
+    var messagesRef = firebase.database().ref().child("mensajes");
+    messagesRef.push({mensaje: this.message, nombre: this.userName});
+    this.message="";
+  }
+  scrollToBottom(){
+    setTimeout(() => {
+      this.content.scrollToBottom(300);
+   }, 1000);
+  }
 }
